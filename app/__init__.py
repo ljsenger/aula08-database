fcf60cf99eefda43727461188b577c2463f41480
Luciano Senger <ljsenger@gmail.com>
2019-11-08 16:09:00 -0300

    v3

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from flask_bootstrap import Bootstrap
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +14,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
+from .models import Usuario, Funcao
+
+admin = Admin(app, name='pegasus', template_mode='bootstrap3')
+admin.add_view(ModelView(Usuario, db.session))
+admin.add_view(ModelView(Funcao, db.session))
 
 if __name__ == "__main__":
     app.run(debug=True)
